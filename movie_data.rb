f2362b9326128dbd683f9edd8e0b3ccd1d633ff8
Steven H <stevenhh@brandeis.edu>
2014-02-04 12:19:53 -0500

    major update-still playing with predict, something is wrong with user_list and movie_list

--- movie_data.rb
+++ movie_data.rb
@@ -1,23 +1,48 @@
 # Author: Steven Hu
 
+require_relative 'movie_test'
+
 class MovieData
 
-	def initialize
-		@master_list = Array.new
-		@popular_list = Hash.new("Movie Doesn't Exist")
+	attr_reader :movie_list, :user_list
+	def initialize(file, test = nil)
+		@master_list = Array.new # will be used as the training list
+		@test_list = Array.new
+		@popular_list = Hash.new(0) # from PA1
+		@movie_list = Hash.new([])
+		@user_list = Hash.new([])
+		load_data(file, test)
 	end
 
-	# load u.data into a master list (master_list) and
-	# prefetch a list it's movies and their popularity
-	def load_data
-		File.open("u.data", "r").each_line do |line|
-			line = line.split
-			@master_list.push([line[0].to_i,line[1].to_i,line[2].to_i])
+	def load_data(file, test)
+		if test.nil? == true
+			load_data_helper(file, 100000, "training")
+		else
+			load_data_helper(file, 80000, "training")
+			load_data_helper(test, 20000, "test")
 		end
-		load_popularity_list # preload @popular_list
-		"Data Loaded" # just so that @popular_list isn't returned automatically
+		# load_popularity_list # preload @popular_list
+		"Data Loaded"
 	end
 
+	def load_data_helper(path, k, type)
+		counter = 0
+		File.open(path, "r").each_line do |line|
+			if counter < k
+				line = line.split
+				if type == "training"
+					@master_list.push([line[0].to_i,line[1].to_i,line[2].to_i])
+					@user_list[line[0].to_i].push([line[1].to_i,line[2].to_i])
+					@movie_list[line[1].to_i].push(line[0].to_i)
+				elsif type == "test"
+					@test_list.push([line[0].to_i,line[1].to_i,line[2].to_i])
+				end
+			end
+			counter += 1
+		end
+	end
+
+	##
 	# helper method for load_popularity_list. method calculates
 	# the popularity of a given movie id
 	def popularity_helper(movie_id)
@@ -29,31 +54,37 @@ class MovieData
 				rating += rate
 			end
 		end
-		# formula puts more weight on the number of people who have
-		# seen the movie vs the ratings of the movie
 		((count * 0.7) + (rating * 0.3)).round(3)
 	end
 
+	##
 	# will load the popularity factors of all the movies. 
 	# method is used in load_data to prefetch @popular_list
 	def load_popularity_list
 		@master_list.each do |user, mov_id, rate|
-			if @popular_list[mov_id] == "Movie Doesn't Exist"
+			if @popular_list[mov_id] == -1
 				@popular_list[mov_id] = popularity_helper(mov_id)
 				end
 		end
 	end
 
+	##
 	# check the popularity factor of a given movie id
 	def popularity(movie_id)
-		@popular_list[movie_id]
+		if @popular_list[movie_id] == 0
+			put "Movie Does Not Exist."
+		else
+			return @popular_list[movie_id]
+		end
 	end
 	
+	##
 	# populates list of movies from most popular to least
 	def popularity_list
 		@popular_list.sort_by {|_,val| -val}
 	end
 	
+	##
 	# presents a similarity factor between two users
 	def similarity(user1, user2)
 		user_list1 = Hash.new(0)
@@ -68,13 +99,9 @@ class MovieData
 		similar_movies(user_list1, user_list2)
 	end
 	
-	# helper method that takes in two hashes of movies
-	# and ratings from two users. output is a similarity factor
-	# that is calculated by the number of movies they have
-	# watched similarly and additional score to those movies
-	# that are rated within +/- 1.
+	##
+	# taken from PA1
 	def similar_movies(ul1, ul2)
-		# intersect the keys of ul1, ul2 to find common movies
 		common = ul1.keys & ul2.keys
 		similar_ratings = 0
 		common.each do |com|
@@ -82,39 +109,77 @@ class MovieData
 				similar_ratings += 1
 			end
 		end
-		# formula takes into account of number of similar movies watched
-		# and +1 on ratings which are close to each other (+/- 1)
 		return common.length + similar_ratings
 	end
 
-	# returns a list of users sorted by popularity factor against user u
+	##
+	# taken from PA1
 	def most_similar(u)
 		similar_list = Hash.new(0)
 		@master_list.each do |user, mov_id, rate|
-			# if 'user' hasn't had smilarity factor calculated yet
-			# and is not user u  
 			if similar_list[user] == 0 and user != u
 				similar_list[user] = similarity(u, user)
 				end
 		end
 		similar_list.sort_by {|_, val| -val}
 	end
+
+	##
+	# returns an array of movies user u watched (taken from training set)
+	def movies(u)
+		u_movies = []
+		temp = @user_list[u]
+		temp.each do |mov_id, rate|
+			u_movies.push(mov_id)
+		end
+		return u_movies
+	end
+
+	##
+	# returns an array of users who watched movie m (taken from training set)
+	def viewers(m)
+		@movie_list[m]
+	end
+
+	##
+	# rating user u gave for movie m in training set
+	def rating(u, m)
+		temp = @user_list[u]
+		temp.each do |mov_id, rate|
+			if mov_id == m
+				return rate
+			end
+		end
+		# returning 0 if user did not rate movie m
+		return 0
+	end
+
+	##
+	# predict method for prediction rating of movie m from user u
+	def predict(u, m)
+		# testing several algorithms at the moment
+	end
+
+	##
+	# run prediction on k lines from the test list and returns a MovieTest object
+	# that is a list of arrays that contain (user, mov_id, rating, and prediction).
+	def run_test(k = @test_list.length)
+		counter = 0
+		results = []
+		@test_list.each do |user, mov_id, rate|
+			if counter < k
+				results.push([user, mov_id, rate, predict(user, mov_id)])
+			end
+			counter += 1
+		end
+		return MovieTest(results)
+	end
+
 end
 
-puts "Running Test for PA1"
-test = MovieData.new
-test.load_data
-puts "Top 10 Most Popular Movies (first ten elements from popularity_list): "
-movies = test.popularity_list
-movies.first(10).each do |a,b|
-	puts "Movie: #{a}, Popularity Factor: #{b}" end
-puts "10 Worse Movies (last ten elements from popularity_list): "
-movies.last(10).each do |a,b|
-	puts "Movie: #{a}, Popularity Factor: #{b}" end
-puts "10 Most Similar Users to User 1 (first ten elements from most_similar(1)): "
-similar = test.most_similar(1)
-similar.first(10).each do |a,b|
-	puts "User: #{a}, Similarity Factor: #{b}" end
-puts "10 Least Similar Users to User 1 (last ten elements from most_similar(1)): "
-similar.last(10).each do |a,b|
-	puts "User: #{a}, Similarity Factor: #{b}" end
+puts "Running Test for PA2"
+test = MovieData.new("u1.base", "u1.test")
+#puts test.viewers(1)
+#puts test.movies(1)
+puts test.user_list[1]
+puts test.rating(1, 58)
---
+++ movie_test.rb
@@ -0,0 +1,49 @@
+# Author: Steven Hu
+
+class MovieTest
+
+	def initialize(results)
+		@results_list = results
+	end
+
+	##
+	# average of the prediction errors (actual rating - prediction)
+	def mean
+		sum = 0.0
+		@results_list.each do |u, m, r, p|
+			sum += (r - p).abs
+		end
+		return sum / @results_list.length
+	end
+
+	##
+	# standard deviation of the prediction errors
+	def stddev
+		average = mean
+		sum = 0.0
+		@results_list.each do |u, m, r, p|
+			sum += ((r - p) - average)**2
+		end
+		return Math.sqrt(sum / @results_list.length)
+	end
+
+	##
+	# root mean square of prediction errors
+	def rms
+		sum = 0.0
+		@results_list.each do |u, m, r, p|
+			sum += (r - p)**2
+		end
+		return Math.sqrt(sum / @results_list.length)
+	end
+
+	##
+	# return a list of arrays containing results (u, m, r, p)
+	def to_a
+		#@results_list.each do |u, m, r, p|
+			#puts "[#{u},#{m},#{r},#{p}]"
+		#end
+		return @results_list
+	end
+
+end
